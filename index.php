36570f7982a189c8b0c065be6eb01d09079d3358
Adidooouuu <79933094+Adidooouuu@users.noreply.github.com>
2021-12-09 15:13:27 +0100

    Mise en place des éléments de la partie accueil, css lié

--- index.php
+++ index.php
@@ -1,14 +1,36 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <script src="assets/js/script.js"></script>
-    <title>Document</title>
-</head>
-<body>
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <!-- les auteurs de la page -->
+      <meta name="author" content="Jean Baradat, Salomé Cliquennois, Lucie Dumas, Adélaïde Machon">
+      <!-- Une petite description du site -->
+      <meta name="description" content="Mini-jeu de puzzle">
+      <link rel="stylesheet" href="assets/css/style.css">
+      <script src="assets/js/script.js"></script>
+      <title>Document</title>
+  </head>
+  <body>
+    <section class="home">
+      <div class="puzzle_game_logo">
+        <h1>- Puzzle Game -</h1>
+      </div>
+      <form class="id_player" action="index.php" method="post">
+        <label for="name_player">Nom du joueur</label>
+        <input id="name_player" type="text" name="name_player" value="" placeholder="Votre nom">
 
-</body>
+        <label for="difficulty">Difficulté</label>
+        <select name="difficulty" id="difficulty">
+          <option value="facile" selected="selected">Facile</option>
+          <option value="moyen">Moyen</option>
+          <option value="difficile">Difficile</option>
+        </select>
+        <div class="start">
+          <button type="button" name="jouer">Jouer</button>
+        </div>
+      </form>
+    </section>
+  </body>
 </html>
