f1f1bdbc2e15007701fa7d05b704973e6706e258
Adidooouuu <79933094+Adidooouuu@users.noreply.github.com>
2021-12-09 22:11:15 +0100

    Peaufinage de la page d'accueil, avec changement de la font des labels + ajout d'un bg au bouton "jouer"

--- index.php
+++ index.php
@@ -11,9 +11,10 @@
       <link rel="preconnect" href="https://fonts.googleapis.com">
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
       <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap" rel="stylesheet">
       <link rel="stylesheet" href="assets/css/style.css">
       <script src="assets/js/script.js"></script>
-      <title>Document</title>
+      <title>Puzzle Game</title>
   </head>
   <body>
     <main>
@@ -23,17 +24,20 @@
                     <h1>- Puzzle Game -</h1>
                 </header>
                 <form class="id_player" action="index.php" method="post">
-                    <label for="name_player">Nom du joueur</label>
+                    <label for="name_player" class="id_player_form_label">Nom du joueur</label>
                     <input id="name_player" type="text" name="name_player" value="" placeholder="Votre nom" required>
 
-                    <label for="difficulty">Difficulté</label>
+                    <label for="difficulty" class="id_player_form_label">Difficulté</label>
                     <select name="difficulty" id="difficulty" required>
                       <option value="facile" selected="selected">Facile</option>
                       <option value="moyen">Moyen</option>
                       <option value="difficile">Difficile</option>
                     </select>
+
                     <div class="start">
-                      <input type="submit" id="start" alt="Jouer" src="/media/examples/login-button.png">
+                      <!-- <label for="start">Jouer</label>
+                      <input type="image" id="start" alt="Jouer" src="assets/img/puzzle.png"> -->
+                      <input type="submit" name="start" value="Jouer" class="id_player_form_input">
                     </div>
                 </form>
             </section>
@@ -42,7 +46,7 @@
 
             </section>
 
-            <section class="">
+            <section class="results">
 
             </section>
         </article>
