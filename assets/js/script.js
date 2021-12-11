0406734f7970bfec24cb90f9c4bc8bb7c7a63142
Ovhas <dovakinbaradat@gmail.com>
2021-12-11 16:16:49 +0100

    mise en place de commentaire dans le script + modification et test le nom du joueur avec indication en cas de problème. des modifs sur la page index.php (des indentations)

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,10 @@
  */
  document.addEventListener("DOMContentLoaded", function() {
 
-
-    /*
-    Je commente le code demain, la il est 3h je suis mort
-    */
-
+    // appel d'un element du DOM
     var form = document.getElementById('home'); 
 
+    // création d'un tableau multidimensionnel associatif pour les données de difficulté
     var tabGame = {
         "facile" : {
            "class" : "puzzle_grid_1",
@@ -25,7 +22,9 @@
         }
     }
 
-    form.addEventListener('submit', function(e){    
+    // Listener pout le submit du formulaire contenu dans le section d'id home
+    form.addEventListener('submit', function(e){
+        // fonction au moment où le formulaire est submit
         clickVerifForm(e);
     }, false);
 
@@ -33,17 +32,54 @@
     function clickVerifForm(e){
         e.preventDefault();
 
-        var name_player = document.getElementById('name_player').value;
+        // récupère l'element DOM (input) d'id name_player
+        var name_player = document.getElementById('name_player');
+        // récupère la valeur du input d'id name_player (soit le nom du joueur renseigné dans le input)
+        var name_player_value = document.getElementById('name_player').value;
+        // test si la donnée (nom du joueur) respecte des conditions
+        if (name_player_value === "" || name_player_value.length > 25 || /\s/g.test(name_player_value)) {
+            // place le focus sur l'input
+            name_player.focus()
+            // met une couleur de fond
+            name_player.style.backgroundColor = "rgba(255, 0, 0, 0.1)";
+            // midification du placeholder
+            name_player.setAttribute("placeholder", "");
+            // return false pour quitter la fonction en cours (clickVerifForm)
+            return false;
+        }
 
+
+        // du form récupère la valeur du select qui est sélectionnée (soit la difficulte selectionne pas le joueur)
         var difficulty = form.querySelector('select');
         var difficulty_valeur = difficulty.options[difficulty.selectedIndex].value;
 
+        // supprime l'element du DOM d'id "home" avec la methode remove (soit toute la zone du formulaire de parametrage du jeu)
         document.getElementById('home').remove();
 
+        /*********************************/
+
+        // appel un element du DOM avec pour id article
         var article = document.getElementById('article');
+
+        /*********************************/
+
+        // création de la page de jeu
+        init_game(name_player_value, difficulty_valeur, article);
+
+    }
+
+    function init_game(name_player_value, difficulty_valeur, article) {
+        // creation d'une balise "section" dans le document
         var section_game = document.createElement("SECTION");
+        // ajout de cette balise "section" dans l'element article
         article.appendChild(section_game);
+        // definition d'attribut avec le nom de l'attribut et son contenu
         section_game.setAttribute("id", "game");
+
+        /**
+         * Les lignes qui suivent font plus ou moins la meme chose que les trois lignes au-dessus.
+         * L'ojectif est de créer une nouvelle arborescence HTML
+         */
 
         var header_game = document.createElement("HEADER");
         section_game.appendChild(header_game);
@@ -54,7 +90,7 @@
 
         var header_game_id = document.getElementById("header_game");
         var title_header_game = header_game_id.querySelector("h1");
-        title_header_game.innerText = "Bonne chance "+name_player+" !";
+        title_header_game.innerText = "Bonne chance "+name_player_value+" !";
 
         var div_puzzle_game = document.createElement("DIV");
         section_game.appendChild(div_puzzle_game);
@@ -64,6 +100,7 @@
         div_puzzle_game.appendChild(div_grid_puzzle_game);
         div_grid_puzzle_game.setAttribute("class", "puzzle_grid "+tabGame[difficulty_valeur]["class"]);
 
+        // en fonction du nombre de défini ici : tabGame[difficulty_valeur]["nbr_grid"]. Creation de plusieurs div
         for (let i = 0; i < tabGame[difficulty_valeur]["nbr_grid"]; i++) {
             var div_puzzle_grid_piece_game = document.createElement("DIV");
             div_grid_puzzle_game.appendChild(div_puzzle_grid_piece_game);
@@ -74,13 +111,13 @@
         div_puzzle_game.appendChild(div_valid_game);
         div_valid_game.setAttribute("class", "valid");
 
+        // le bouton de validation du puzzle
         var input_valid_game = document.createElement("INPUT");
         div_valid_game.appendChild(input_valid_game);
         input_valid_game.setAttribute("type", "submit");
         input_valid_game.setAttribute("name", "valid");
         input_valid_game.setAttribute("value", "Valider");
         input_valid_game.setAttribute("class", "puzzle_validation_btn");
-
     }
 
 });
--- index.php
+++ index.php
@@ -1,51 +1,51 @@
 <!DOCTYPE html>
 <html lang="fr">
   <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <!-- les auteurs de la page -->
-      <meta name="author" content="Jean Baradat, Salomé Cliquennois, Lucie Dumas, Adélaïde Machon">
-      <!-- Une petite description du site -->
-      <meta name="description" content="Mini-jeu de puzzle">
-      <link rel="preconnect" href="https://fonts.googleapis.com">
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-      <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
-      <link href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap" rel="stylesheet">
-      <link rel="stylesheet" href="assets/css/style.css">
-      <title>Puzzle Game</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- les auteurs de la page -->
+    <meta name="author" content="Jean Baradat, Salomé Cliquennois, Lucie Dumas, Adélaïde Machon">
+    <!-- Une petite description du site -->
+    <meta name="description" content="Mini-jeu de puzzle">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Puzzle Game</title>
   </head>
   <body>
     <main>
-        <article id="article">
+      <article id="article">
 
-            <section id="home">
-                <header id="header_home">
-                    <h1>- Puzzle Game -</h1>
-                </header>
-                <form class="id_player" action="index.php" method="get">
-                    <label for="name_player" class="id_player_form_label">Nom du joueur</label>
-                    <input id="name_player" type="text" name="name_player" value="" placeholder="Votre nom">
+        <section id="home">
+          <header id="header_home">
+            <h1>- Puzzle Game -</h1>
+          </header>
+          <form class="id_player">
+            <label for="name_player" class="id_player_form_label">Nom du joueur</label>
+            <input id="name_player" type="text" name="name_player" value="" placeholder="Votre nom">
+            
+            <label for="difficulty" class="id_player_form_label">Difficulté</label>
+            <select name="difficulty" id="difficulty">
+              <option value="facile" selected="selected">Facile</option>
+              <option value="moyen">Moyen</option>
+              <option value="difficile">Difficile</option>
+            </select>
 
-                    <label for="difficulty" class="id_player_form_label">Difficulté</label>
-                    <select name="difficulty" id="difficulty">
-                      <option value="facile" selected="selected">Facile</option>
-                      <option value="moyen">Moyen</option>
-                      <option value="difficile">Difficile</option>
-                    </select>
+            <div class="start">
+              <input type="submit" value="Jouer" id="id_player_form_submit">
+            </div>
+          </form>
+        </section>
 
-                    <div class="start">
-                      <input type="submit" value="Jouer" id="id_player_form_submit">
-                    </div>
-                </form>
-            </section>
+          <!-- <section id="results"></section> -->
+      </article>
 
-            <!-- <section id="results"></section> -->
-        </article>
+      <footer>
 
-        <footer>
-
-        </footer>
+      </footer>
     </main>
 
     <aside>
