c8263edb6d03b1af9add43c61ab2d4e1c5ec2fa7
Salomé <bonjour@salome-c.xyz>
2021-12-12 23:07:05 +0100

    simplification du script avec jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,7 @@
 /**
  * On attend que le DOM soit totalement chargé
  */
- document.addEventListener("DOMContentLoaded", function() {
-
-    // appel d'un element du DOM
-    var form = document.getElementById('home');
+ $(document).ready(function() {
 
     // création d'un tableau multidimensionnel associatif pour les données de difficulté
     var tabGame = {
@@ -36,49 +33,49 @@
     const Reg_exp = /^([A-Za-zÀ-ÖØ-öø-ÿ]{3,25})$/;
 
     // Listener pout le submit du formulaire contenu dans le section d'id home
-    form.addEventListener('submit', function(e){
+    $('#home').on('submit', function(e){
         // fonction au moment où le formulaire est submit
         clickVerifForm(e);
-    }, false);
+        return false;
+    });
 
 
     function clickVerifForm(e){
         e.preventDefault();
 
         // récupère l'element DOM (input) d'id name_player
-        var name_player = document.getElementById('name_player');
+        var name_player = $('#name_player');
         // récupère la valeur du input d'id name_player (soit le nom du joueur renseigné dans le input)
-        var name_player_value = document.getElementById('name_player').value;
+        var name_player_value = $('#name_player').val();
         // test si la donnée (nom du joueur) respecte des conditions
         if (!Reg_exp.test(name_player_value)) {
             // place le focus sur l'input
-            name_player.focus()
+            name_player.focus();
             // met une couleur de fond
-            name_player.style.backgroundColor = "rgba(255, 0, 0, 0.1)";
-            // défini la valeur de name_player en une chaine de caractere vide
-            name_player.value = "";
+            name_player.css("background-color", "rgba(255, 0, 0, 0.1)");
+            // définit la valeur de name_player en une chaine de caractere vide
+            name_player.val("");
             // modification du placeholder
-            name_player.setAttribute("placeholder", "");
-            //Envoi d'un message d'erreur dans le paragraphe dédié (en le vidant avant s'il contenait déjà un message)
-            document.getElementsByClassName("id_player")[0].style.padding = "1.83rem 6rem";
-            document.getElementById("error_msg").innerHTML = "";
-            document.getElementById("error_msg").innerHTML = "Seulement des lettres ! :) <br> De 3 à 25 caractères !";
+            name_player.attr("placeholder", "");
+            // Envoi d'un message d'erreur dans le paragraphe dédié (en le vidant avant s'il contenait déjà un message)
+            $(".id_player").first().css("padding", "1.83rem 6rem");
+            $("#error_msg").html("");
+            $("#error_msg").html("Seulement des lettres ! :) <br> De 3 à 25 caractères !");
             // return false pour quitter la fonction en cours (clickVerifForm)
             return false;
         }
 
 
-        // du form récupère la valeur du select qui est sélectionnée (soit la difficulte selectionne pas le joueur)
-        var difficulty = form.querySelector('select');
-        var difficulty_valeur = difficulty.options[difficulty.selectedIndex].value;
+        // du form récupère la valeur du select qui est sélectionné (soit la difficulte selectionne par le joueur)
+        var difficulty_valeur = $("#home select option:selected").val();
 
         // supprime l'element du DOM d'id "home" avec la methode remove (soit toute la zone du formulaire de parametrage du jeu)
-        document.getElementById('home').remove();
+        $('#home').remove();
 
         /*********************************/
 
         // appel un element du DOM avec pour id article
-        var article = document.getElementById('article');
+        var article = $('#article');
 
         /*********************************/
 
@@ -89,54 +86,36 @@
 
     function init_game(name_player_value, difficulty_valeur, article) {
         // creation d'une balise "section" dans le document
-        var section_game = document.createElement("SECTION");
+        var section_game = $("<section id='game'></section>");
         // ajout de cette balise "section" dans l'element article
-        article.appendChild(section_game);
-        // definition d'attribut avec le nom de l'attribut et son contenu
-        section_game.setAttribute("id", "game");
+        article.append(section_game);
 
         /**
-         * Les lignes qui suivent font plus ou moins la meme chose que les trois lignes au-dessus.
-         * L'ojectif est de créer une nouvelle arborescence HTML
+         * Les lignes qui suivent font plus ou moins la meme chose que les deux lignes au-dessus.
+         * L'objectif est de créer une nouvelle arborescence HTML
          */
 
-        var header_game = document.createElement("HEADER");
-        section_game.appendChild(header_game);
-        header_game.setAttribute("id", "header_game");
+        var header_game = $("<header id='header_game'></header>");
+        section_game.append(header_game);
 
-        var h1 = document.createElement("H1");
-        header_game.appendChild(h1);
+        header_game.append("<h1>À vous de jouer "+name_player_value+" !</h1>");
 
-        var header_game_id = document.getElementById("header_game");
-        var title_header_game = header_game_id.querySelector("h1");
-        title_header_game.innerText = "À vous de jouer "+name_player_value+" !";
+        var div_puzzle_game = $("<div class='puzzle'></div>");
+        section_game.append(div_puzzle_game);
 
-        var div_puzzle_game = document.createElement("DIV");
-        section_game.appendChild(div_puzzle_game);
-        div_puzzle_game.setAttribute("class", "puzzle");
+        var div_grid_puzzle_game = $("<div class='puzzle_grid "+tabGame[difficulty_valeur]["class"]+"'></div>");
+        div_puzzle_game.append(div_grid_puzzle_game);
 
-        var div_grid_puzzle_game = document.createElement("DIV");
-        div_puzzle_game.appendChild(div_grid_puzzle_game);
-        div_grid_puzzle_game.setAttribute("class", "puzzle_grid "+tabGame[difficulty_valeur]["class"]);
-
-        // en fonction du nombre de défini ici : tabGame[difficulty_valeur]["nbr_grid"]. Creation de plusieurs div
+        // en fonction du nombre de cases défini ici : tabGame[difficulty_valeur]["nbr_grid"]. Creation de plusieurs div
         for (let i = 0; i < tabGame[difficulty_valeur]["nbr_grid"]; i++) {
-            var div_puzzle_grid_piece_game = document.createElement("DIV");
-            div_grid_puzzle_game.appendChild(div_puzzle_grid_piece_game);
-            div_puzzle_grid_piece_game.setAttribute("class", "puzzle_grid_piece");
+            div_grid_puzzle_game.append("<div class='puzzle_grid_piece'></div>");
         }
 
-        var div_valid_game = document.createElement("DIV");
-        div_puzzle_game.appendChild(div_valid_game);
-        div_valid_game.setAttribute("class", "valid");
+        var div_valid_game = $("<div class='valid'></div>");
+        div_puzzle_game.append(div_valid_game);
 
         // le bouton de validation du puzzle
-        var input_valid_game = document.createElement("INPUT");
-        div_valid_game.appendChild(input_valid_game);
-        input_valid_game.setAttribute("type", "submit");
-        input_valid_game.setAttribute("name", "valid");
-        input_valid_game.setAttribute("value", "Valider");
-        input_valid_game.setAttribute("id", "puzzle_validation_btn");
+        div_valid_game.append("<input type='submit' name='valid' value='Valider' id='puzzle_validation_btn' />");
     }
 
 });
--- index.php
+++ index.php
@@ -53,7 +53,7 @@
 
     </aside>
 
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script type="text/javascript" src="assets/js/script.js"></script>
-    <script src='https://code.jquery.com/jquery-3.3.1.min.js'></script>
   </body>
 </html>
